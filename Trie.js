760ba4aa84ae64fd41d65cfa56da5133ba10fda7
corykacal <corykacal@utexas.edu>
2019-02-07 09:39:24 -0600

    started trie.js. added trienode structure

---
+++ Trie.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+function TrieNode(key) {
+    this.character = character;
+    this.parent = null;
+    this.children = {};
+    this.wordEnd = false;
+    this.childCount = 0
+}
+
+TrieNode.prototype.contains = function(character) {
+
+
+};
+
+
+TrieNode.prototype.get = function(character) {
+
+
+};
+
+
+TrieNode.prototype.remove = function(character) {
+
+
+};
+
+
+
+TrieNode.prototype.isEnd = function(character) {
+
+
+};
+
+
+
+TrieNode.prototype.isWordEnd = function(character) {
+
+
+};
