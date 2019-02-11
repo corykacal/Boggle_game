1c91a93ec240d4d8cd0c3b1fee6703e591de2ac6
corykacal <corykacal@utexas.edu>
2019-02-11 13:56:51 -0600

    added add method to trie

--- Trie.js
+++ Trie.js
@@ -3,22 +3,31 @@
 
 
 
+/*
+ * Constructor for node in tree
+ *
+ */
 function TrieNode(key) {
     this.character = character;
     this.parent = null;
-    this.children = {};
+    //Array to get it working. impliment a dictionary later
+    //Preallocated array has time complexity but horrible space
+    //Array has bad time but good space
+    //dictionary has both
+    this.children = [];
     this.wordEnd = false;
     this.childCount = 0
 }
 
 TrieNode.prototype.contains = function(character) {
-
-
+    return children.includes(character);
 };
 
+TrieNode.prototype.add = function(character) {
+    children.push(character);
+};
 
 TrieNode.prototype.get = function(character) {
-
 
 };
 
@@ -41,3 +50,7 @@ TrieNode.prototype.isWordEnd = function(character) {
 
 
 };
+
+
+
+
